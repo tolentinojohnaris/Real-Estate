ea74b7a7d25439640e7b4b4bc7f9b8eea237c972
tolentinojohnaris <tolentinojohnarisiv4@gmail.com>
2023-03-19 02:08:46 +0800

    March 19 2023
    Added Contact Overlay
    Fixed Sidebar script

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 //sidebar
 
-function sidebarBtnFunction(){
+function sidebarShowBtn(){
     const sidebar = document.getElementById("sidebar");
-    sidebar.classList.toggle("hidden");
+    sidebar.classList.toggle("sidebar_show");
+    sidebar.classList.remove("sidebar_hide");
 }
 
+function sidebarHideBtn(){
+    const hideSidebar = document.getElementById("sidebar");
+    sidebar.classList.toggle("sidebar_hide");
+    sidebar.classList.remove("sidebar_show");
+}
+
+//contact overlay
+function contactOverlayShow(){
+    const showContactOverlay = document.getElementById("contact_overlay");
+    showContactOverlay.classList.toggle("contact_overlay_show");
+    showContactOverlay.classList.remove("contact_overlay_hide");
+    console.log("contact overlay clicked");
+}
+
+function contactOverlayHide(){
+    const hideContactOverlay = document.getElementById("contact_overlay");
+    hideContactOverlay.classList.toggle("contact_overlay_hide");
+    hideContactOverlay.classList.remove("contact_overlay_show");
+}
 
 //testimonial JS
 const testimonialCol = document.querySelector(".testimonial-col");
